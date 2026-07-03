a839a5cefee0c26939cce7237fbb03e4ebd4c10c
ckck912ck-lang <ckck912ck@gmail.com>
2026-07-03 15:40:13 +0900

    Update react-router to version 7.18.1 and refactor routing in App component; enhance Write and View components with modify and delete functionality

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,9 +1,9 @@
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router";
+import { Link, useParams, useNavigate } from "react-router";
 
-export default function View() {
+export default function View({ handleModify }) {
   const [content, setContent] = useState({
     writer: "",
     title: "",
@@ -13,18 +13,20 @@ export default function View() {
   const [isError, setIsError] = useState(false);
 
   const { id } = useParams();
+  let navigate = useNavigate();
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/view?id=${id}`)
       .then(response => {
         console.log(response.data); //[{..}]
-        // setContent(response.data);
-
-        //data가 없거나 data의 배열의 갯수가 0과 같다면
+        //setContent(response.data);
+        //data가 없거나 data의 배열의 개수가 0가 같다면
         if (!response.data || response.data.length === 0) {
           setIsError(true);
           return;
         }
+
         const data = response.data[0];
 
         setContent({
@@ -36,6 +38,7 @@ export default function View() {
       })
       .catch(error => {
         console.error(error);
+        setIsError(true);
       })
       .finally(() => {
         console.log("요청완료");
@@ -46,14 +49,31 @@ export default function View() {
     return (
       <div>
         <p>잘못된 접근입니다.</p>
-        <p>다시 확인해주세요.</p>
+        <p>다시확인해주세요</p>
         <Link to="/" className="btn btn-primary">
           홈으로 이동
         </Link>
       </div>
     );
   }
-
+  const handleClick = () => {
+    handleModify(id);
+  };
+  const handleDelete = () => {
+    if (window.confirm("정말 삭제할까요")) {
+      axios
+        .post("http://localhost:3000/delete", {
+          id: id,
+        })
+        .then(() => {
+          navigate("/");
+        })
+        .catch(error => {
+          console.error(error);
+        })
+        .finally(() => {});
+    }
+  };
   return (
     <>
       <h2>{content.title}</h2>
@@ -68,8 +88,12 @@ export default function View() {
         <Link to="/" className="btn btn-primary">
           홈
         </Link>
-        <Button variant="secondary">수정</Button>
-        <Button variant="danger">삭제</Button>
+        <Button variant="secondary" onClick={handleClick}>
+          수정
+        </Button>
+        <Button variant="danger" onClick={handleDelete}>
+          삭제
+        </Button>
       </div>
     </>
   );
